c8e10779e79b186a6026022286eed9b94f891f93
abhinavpy <abhinavpy.171co103@nitk.edu.in>
2019-01-16 15:38:17 +0530

    Finished code for program 3, file input

--- Lab2/src/FileInput.java
+++ Lab2/src/FileInput.java
@@ -1,3 +1,41 @@
+package com.journaldev.files;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class FileInput {
 
+    private static void writeUsingFileWriter(String data) {
+        File file = new File("/home/student/Desktop/FileWriter.txt");
+        FileWriter fr = null;
+        try {
+            fr = new FileWriter(file);
+            fr.write(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            //close resources
+            try {
+                fr.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public static void main(String args[]) {
+        String s = new String();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter text to be input into the file FileWriter.txt\n");
+        String data = sc.nextLine();
+        writeUsingFileWriter(data);
+    }
 }
--- Lab2/src/Sample.java
+++ Lab2/src/Sample.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+
 public class Sample {
-    Sample() {
-        System.out.println("Constructor \n");
+    Sample(String s) {
+        System.out.println("Inside the constructor: " + s + "\n");
     }
 
     public static void main(String args[]) {
-        Sample obj1 = new Sample();
+
+        Scanner sc = new Scanner(System.in);
+        String s = new String();
+        System.out.println("Enter text\n");
+        s = sc.next();
+
+        Sample obj1 = new Sample(s);
     }
 }
